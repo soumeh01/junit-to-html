985bebf792791946c4edb348e4de5cd25b284d3a
Sourabh Mehta <sourabh.mehta@arm.com>
2023-08-01 14:52:49 +0200

    Error handling updated

--- scripts/merge_test_reports.py
+++ scripts/merge_test_reports.py
@@ -13,21 +13,24 @@ from pathlib import Path
 class ReportsMerger:
     __total_test_count = 0
 
-    def merge(self, dir:str, mergefile:str):
-        mergedxml = JUnitXml()
+    def merge(self, dir:str, mergeFile:str) -> int:
+        mergedXML = JUnitXml()
         logging.info(f"Collecting test reports with pattern '{dir}/**/*.xml' ...")
-
-        for f in iglob(dir+"/**/*.xml", recursive=True):
+        files = iglob(dir+"/**/*.xml", recursive=True)
+        if len(files) < 1:
+            return 1
+        for f in files:
             filename=Path(f).stem
             index=filename.find("report-")
             if index == -1:
+                logging.error('No files found under: '+ str(dir))
                 continue
             xmTestReport = self.__udpate_testsuite_name(f, filename[index + len("report-"):].replace("-", "."))
-            mergedxml = mergedxml + xmTestReport
-        mergedxml.tests = self.__total_test_count
+            mergedXML = mergedXML + xmTestReport
+        mergedXML.tests = self.__total_test_count
 
-        logging.info(f"Writing merged report to '{mergefile}' ...")
-        mergedxml.write(mergefile)
+        logging.info(f"Writing merged report to '{mergeFile}' ...")
+        mergedXML.write(mergeFile)
 
     def __udpate_testsuite_name(self, filename: str, prefix: str):
         xml = JUnitXml.fromfile(filename)
@@ -39,19 +42,21 @@ class ReportsMerger:
                     case.classname = suite.name
         return xml
 
-def main():
-    logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+def main() -> int:
+    try:
+        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        parser = ArgumentParser()
+        parser.add_argument("-i", "--input", default=".", help="Directory to search for test results.")
+        parser.add_argument("-o", "--outfile", default="Merged.xml", help="Merged Output file path.")
+        args = parser.parse_args()
 
-    parser = ArgumentParser()
-    parser.add_argument("-i", "--input", default=".", help="Directory to search for test results.")
-    parser.add_argument("-o", "--outfile", default="Merged.xml", help="Merged Output file path.")
-    args = parser.parse_args()
+        logging.info("Merging test reports...")       
+        merger = ReportsMerger()
+        return merger.merge(args.input, args.outfile)
 
-    logging.info("Merging test reports...")
-    
-    merger = ReportsMerger()
-    merger.merge(args.input, args.outfile)
-
+    except Exception as err:
+        logging.error('Failed to merge files: '+ str(err))
+        return 1
 
 if __name__ == '__main__':
     sys.exit(main())
